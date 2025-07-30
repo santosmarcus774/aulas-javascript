88f9c3ce2114bc278ba57266bed03cdf588e4247
santosmarcus774 <santosmarcus774@gmail.com>
2025-07-30 15:26:52 -0300

    meu-script

---
+++ js/02-meu-script.js
@@ -0,0 +1,11 @@
+/*Exercicio*/
+
+
+//Crie uma variavel chamada "uc" e coloque nela o numero da uc que estamos
+
+//crie uma variavel chamada  "competencia" e coloque nela uma frase que represente o que estamos estudando nesta UC.
+
+//Monte e mostre uma frase no console exibindo a UC e a competencia .Exemplo: Estamos na UC2 estudando Front-End com JavaScript.
+
+ let uc(2);
+ const competencia="Estamos na UC2 estudando  Front-End com JavaScript"
